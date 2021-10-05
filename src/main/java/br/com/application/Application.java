39b5ca7dd671de93f9a4ec10b6701c5ec852c9e7
rospiel <rospielberg@gmail.com>
2021-10-05 16:18:47 -0300

    config inicial bem como de-para dos services

--- src/main/java/br/com/application/Application.java
+++ src/main/java/br/com/application/Application.java
@@ -2,9 +2,13 @@ package br.com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import java.util.TimeZone;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
 
